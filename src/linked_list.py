cad81b1ecc7f6ab4a3b366bcdbb4dc112d2c07dc
Luis Rosario <luisprooc@gmail.com>
2021-04-08 17:49:51 -0300

    Linked list finished

--- src/linked_list.py
+++ src/linked_list.py
@@ -23,7 +23,7 @@ class LinkedList(object):
     def append(self,new_element):
         current = self.head
 
-        if self.head:
+        if current:
             while current.next:
                 current = current.next
 
@@ -34,6 +34,13 @@ class LinkedList(object):
 
     
     def get_position(self,position):
+
+        """
+        Get an element from a particular position.
+        Assume the first position is "1".
+        Return "None" if position is not in the list.
+        """
+
         current = self.head
         next = 1
 
@@ -47,6 +54,71 @@ class LinkedList(object):
 
         # If position not exist return None
         return Element( None )
+
+
+    def insert(self, new_element, position):
+
+        """
+        Insert a new node at the given position.
+        Assume the first position is "1".
+        Inserting at position 3 means between
+        the 2nd and 3rd elements.
+        
+        """
+
+        current = self.head
+        next = 1
+
+        if position == 1:
+            new_element.next = self.head.next
+            self.head = new_element
+
+            return None
+
+        while current:
+
+            if current.next:
+                if position == next + 1:
+                    new_element.next = current.next 
+                    current.next = new_element
+
+
+            current = current.next
+            next += 1
+
+
+    def delete(self, value):
+        """Delete the first node with a given value."""
+        
+
+        current = self.head
+
+        # If the value is equal to head, replace it
+        if current.next and current.value == value:
+            self.head = current.next 
+            return None
+
+        while current:
+            
+            if current.next:
+                if current.next.value == value:
+                    current.next = current.next.next
+
+            current = current.next
+
+
+    def print_items(self):
+        current = self.head
+        position = 1
+
+        while current:
+
+            print(current.value, "pos: {0} ".format(position))
+            current = current.next
+            position += 1
+
+
+        return None
 
 
         
@@ -64,5 +136,6 @@ ll = LinkedList(e1)
 ll.append(e2)
 ll.append(e3)
 
-print (ll.get_position(5).value)
 
+ll.insert(e4,3)
+ll.print_items()
